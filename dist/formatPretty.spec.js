f4c60325430ce9c709042bbe2852fb8f23116fa6
James Harvey <james.harvey@equiem.com.au>
2018-10-19 16:03:14 +1100

    Fix tests for formatPretty

--- dist/formatPretty.spec.js
+++ dist/formatPretty.spec.js
@@ -16,35 +16,38 @@ const chai_1 = require("chai");
 const chai_as_promised_1 = __importDefault(require("chai-as-promised"));
 const mocha_typescript_1 = require("mocha-typescript");
 const sinon_1 = __importDefault(require("sinon"));
-const formatWhitelist_1 = require("./formatWhitelist");
+const formatPretty_1 = require("./formatPretty");
 chai_1.use(chai_as_promised_1.default);
 // tslint:disable:no-unsafe-any
 // tslint:disable:no-unused-expression
 /**
  * Tests for the Environment.
  */
-let FormatWhitelistSpec = class FormatWhitelistSpec {
+let FormatPrettySpec = class FormatPrettySpec {
     before() {
         this.clock = sinon_1.default.useFakeTimers({ now: new Date() });
     }
-    appendTimestamp() {
+    prettyFormat() {
         const changes = [
             { type: "FIELD_REMOVED", description: "User.uuid was removed" },
-            { type: "FIELD_REMOVED", description: "User.name was removed" },
+            { type: "TYPE_REMOVED", description: "User was removed" },
         ];
-        const output = formatWhitelist_1.formatWhitelist(changes).split("\n").map((line) => JSON.parse(line));
-        chai_1.expect(output[0]).to.deep.eq(Object.assign({}, changes[0], { timestamp: this.clock.Date().getTime() }));
-        chai_1.expect(output[1]).to.deep.eq(Object.assign({}, changes[1], { timestamp: this.clock.Date().getTime() }));
+        const output = formatPretty_1.formatPretty(changes);
+        chai_1.expect(output).to.include("Breaking changes were detected");
+        chai_1.expect(output).to.include("FIELD_REMOVED");
+        chai_1.expect(output).to.include("User.uuid was removed");
+        chai_1.expect(output).to.include("TYPE_REMOVED");
+        chai_1.expect(output).to.include("User was removed");
     }
 };
 __decorate([
-    mocha_typescript_1.test("append current timestamp to each breaking change"),
+    mocha_typescript_1.test("pretty format breaking changes"),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", void 0)
-], FormatWhitelistSpec.prototype, "appendTimestamp", null);
-FormatWhitelistSpec = __decorate([
+], FormatPrettySpec.prototype, "prettyFormat", null);
+FormatPrettySpec = __decorate([
     mocha_typescript_1.suite(mocha_typescript_1.timeout(300), mocha_typescript_1.slow(50))
-], FormatWhitelistSpec);
-exports.FormatWhitelistSpec = FormatWhitelistSpec;
+], FormatPrettySpec);
+exports.FormatPrettySpec = FormatPrettySpec;
 //# sourceMappingURL=formatPretty.spec.js.map

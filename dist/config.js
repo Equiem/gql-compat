d9be7f73e8b9eef964d1f91edd0f27c8e858d470
James Harvey <james.harvey@equiem.com.au>
2018-10-29 10:55:39 +1100

    build: remove dist + checks

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * The opinionated name of the ignore file.
- */
-exports.IGNORE_FILE = ".gql-compat-ignore";
-//# sourceMappingURL=config.js.map
